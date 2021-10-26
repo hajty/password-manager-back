0b6e8cec9f47017d216cbe8f452be1a4f4d45522
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2021-10-26 21:03:00 +0200

    changed CORS for heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://hajtys-password-manager.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
